a5fa8d65e2ce334008657bf4458eb7c3b245c21c
Juan Erazo <fersot100@yahoo.com>
2017-10-31 13:05:34 -0700

    Initial Template commit - Untested

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+//ReactDOM.render(, document.getElementById('app'));
